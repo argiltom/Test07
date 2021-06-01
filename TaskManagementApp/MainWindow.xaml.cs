b357599dfd52a9016afb3de9fb79ad12d79e6ea7
argiltom <tomo0118@live.jp>
2021-06-01 17:14:43 +0900

    メイン画面の調整
    メイン画面の調整

--- TaskManagementApp/MainWindow.xaml.cs
+++ TaskManagementApp/MainWindow.xaml.cs
@@ -13,20 +13,28 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace TaskManagementApp
 {
+    
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        ///<para>外部変数:ID=3</para>
+        ///<para>現在時刻を格納する</para>
+        ///<para>利用範囲:システム全体</para>
+        /// </summary>
+        public DateTime nowTime=DateTime.Now;
         public MainWindow()
         {
 
             InitializeComponent();
             Summary sum = new Summary();
-            DateTime datetime = DateTime.Now;
-            nowTimeView.Text = datetime.ToString();
+            nowTimeView.Text = nowTime.ToString();
 
 
         }
