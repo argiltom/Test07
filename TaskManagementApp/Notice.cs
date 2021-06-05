2878d4b8e53cd3e93bc2c92f60551ae7c21e530b
Masui1221 <82438997+Masui1221@users.noreply.github.com>
2021-06-05 12:06:54 +0900

    ddddd
    C3Notice

---
+++ TaskManagementApp/Notice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp
+{
+    class Notice
+    {
+
+        public void NoticeON()//通知がONだったらM3-1(タスクのポップアップ通知)とM3-2(タスクの枠色変更)モジュールを呼ぶ
+        {
+
+
+        }
+
+
+        public void NoticePopUp()//タスク通知M表示
+        {
+            
+
+        }
+
+        public void TaskColor()//タスク枠色変更
+        {
+
+        }
+    }
+}
