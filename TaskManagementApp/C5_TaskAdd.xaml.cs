48253760a870384b949795e43da4a0d2ce9f7095
PEI53 <al19053@shibaura-it.ac.jp>
2021-06-08 17:07:13 +0900

    入力についてのコード（追加）

--- TaskManagementApp/C5_TaskAdd.xaml.cs
+++ TaskManagementApp/C5_TaskAdd.xaml.cs
@@ -19,20 +19,41 @@ namespace TaskManagementApp
     /// </summary>
     public partial class C5_TaskAdd : Window
     {
+        string summary = "";
+        string info = "";
+        string priority = "0";
+        DateTime deadline = DateTime.Now;
         public C5_TaskAdd()
         {
             InitializeComponent();
             this.DataContext = new C5_PriorityList();
+            AccessorTaskList atl = new AccessorTaskList();
+            atl.InitializeJsonData();
+            AccessorOptionData aod = new AccessorOptionData();
+            aod.InitializeJsonData();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            this.summary = taskSummary.Text;
+        }
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.info = taskInfo.Text;
         }
 
         private void combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            this.priority = taskPriority.SelectedValue.ToString();
+        }
 
+        private void taskDeadline_ValueChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.deadline = taskDeadline.SelectedDate.Value;
+        }
+
+        private void Add_Click(object sender, SelectionChangedEventArgs e)
+        {
         }
     }
 }
