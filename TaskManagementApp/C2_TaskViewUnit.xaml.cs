03af2d3bdb7298719bd2025f9b44089d4f75fd6f
argiltom <tomo0118@live.jp>
2021-06-11 12:13:51 +0900

    色々した
    色々した

--- TaskManagementApp/C2_TaskViewUnit.xaml.cs
+++ TaskManagementApp/C2_TaskViewUnit.xaml.cs
@@ -20,9 +20,45 @@ namespace TaskManagementApp
     /// </summary>
     public partial class C2_TaskViewUnit : UserControl
     {
+        /// <summary>
+        /// xamlのsummaryTextと連携(依存関係プロパティ)
+        /// </summary>
+        public static readonly DependencyProperty SummaryTextProperty =
+            DependencyProperty.Register(
+                    "SummaryText",
+                    typeof(string),
+                    typeof(C2_TaskViewUnit),//このプロパティを所有する型=このクラス名
+                    new PropertyMetadata("summary")//初期値
+                );
+        /// <summary>
+        /// SummaryTextへのアクセサー
+        /// </summary>
+        public string SummaryText {
+            get
+            {
+                return (string)GetValue(SummaryTextProperty);
+            }
+            set
+            {
+                SetValue(SummaryTextProperty, value);
+            }
+        }
+        /// <summary>
+        /// xamlのBtnTaskComplishedと連携(依存関係プロパティ)
+        /// </summary>
+        public static readonly DependencyProperty BtnTaskComplishedProperty =
+            DependencyProperty.Register("BtnTaskComplished", typeof(ICommand), typeof(C2_TaskViewUnit), new PropertyMetadata(null));
+
+        public ICommand BtnTaskComplished
+        {
+            get { return (ICommand)GetValue(BtnTaskComplishedProperty); }
+            set { SetValue(BtnTaskComplishedProperty, value); }
+        }
+
         public C2_TaskViewUnit()
         {
             InitializeComponent();
+            
         }
     }
 }
