2370f8b1d6364b619129f51893ba7f2357d90953
argiltom <tomo0118@live.jp>
2021-06-19 10:27:50 +0900

    1秒ごとにタスク更新を自動で行うようにした
    1秒ごとにタスク更新ができるようにした

--- TaskManagementApp/MainWindow.xaml.cs
+++ TaskManagementApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,8 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
+using System.Windows.Threading;
 
 namespace TaskManagementApp
 {
@@ -39,17 +39,29 @@ namespace TaskManagementApp
             AccessorOptionData aod = new AccessorOptionData();
             aod.InitializeJsonData();
             //TaskView taskview = new TaskView(taskViewGrid);
-            TaskViewStackPanelController.UpdateTaskViewStakPanel(SPtaskView, Sort.SortImportance(AccessorTaskList.taskList));
             Console.WriteLine("mainWindow稼働中");
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Tick += new EventHandler(MainWindowUpdate);
+            dispatcherTimer.Start();
         }
 
         private void addTaskButton_Click(object sender, RoutedEventArgs e)
         {
             C5_TaskAdd ta = new C5_TaskAdd();
             ta.Show();
-            TaskViewStackPanelController.UpdateTaskViewStakPanel(SPtaskView, Sort.SortImportance(AccessorTaskList.taskList));
+            
         }
+        /// <summary>
+        /// C2 MainWindowでタスク表示を常時更新させる処理
+        /// </summary>
+        private void MainWindowUpdate(object sender,EventArgs eventArgs)
+        {
 
+                TaskViewStackPanelController.UpdateTaskViewStakPanel(SPtaskView, Sort.SortImportance(AccessorTaskList.taskList));
+                //Console.WriteLine("アタランテ");
+            
+        }
         private void editTaskButton_Click(object sender, RoutedEventArgs e)
         {
 
