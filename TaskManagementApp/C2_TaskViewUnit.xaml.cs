7de0fae9453b385048c04acd5b3160857e7be423
argiltom <tomo0118@live.jp>
2021-06-22 13:22:09 +0900

    push
    push

--- TaskManagementApp/C2_TaskViewUnit.xaml.cs
+++ TaskManagementApp/C2_TaskViewUnit.xaml.cs
@@ -21,6 +21,10 @@ namespace TaskManagementApp
     public partial class C2_TaskViewUnit : UserControl
     {
         /// <summary>
+        /// コンストラクタから受け取ったものを格納するもの
+        /// </summary>
+        Task task;
+        /// <summary>
         /// xamlのsummaryTextと連携(依存関係プロパティ)
         /// </summary>
         public static readonly DependencyProperty SummaryTextProperty =
@@ -105,14 +109,47 @@ namespace TaskManagementApp
             }
         }
 
+        /// <summary>
+        /// xamlのTaskNoticeColorと連携(依存関係プロパティ)
+        /// </summary>
+        public static readonly DependencyProperty TaskNoticeColorProperty =
+            DependencyProperty.Register(
+                    "TaskNoticeColor",
+                    typeof(string),
+                    typeof(C2_TaskViewUnit),//このプロパティを所有する型=このクラス名
+                    new PropertyMetadata("#0000")//初期値 #ARGB
+                );
+        /// <summary>
+        /// TaskNoticeColorへのアクセサー
+        /// </summary>
+        public string TaskNoticeColor
+        {
+            get
+            {
+                return (string)GetValue(TaskNoticeColorProperty);
+            }
+            set
+            {
+                //nullなら透明のまま
+                if(value!=null) SetValue(TaskNoticeColorProperty, value);
+            }
+        }
 
+
+        public void compleatedTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            AccessorTaskList atl = new AccessorTaskList();
+            atl.RemoveTaskList(task);
+        }
 
         public C2_TaskViewUnit(Task task)
         {
             InitializeComponent();
+            this.task = task;
             SummaryText = task.taskSummary;
             TaskLimitText ="期限："+task.taskLimit;
             TaskImportanceText = "重要度:" +task.taskPriority;
+            TaskNoticeColor = task.taskNoticeColor;
         }
     }
 }
--- TaskManagementApp/Task.cs
+++ TaskManagementApp/Task.cs
@@ -17,11 +17,11 @@ namespace TaskManagementApp
         public string taskLimit { set; get; }
         public int taskPriority { set; get; }
         /// <summary>
-        /// C6 タスクの色(ユーザー指定) (使わないかも)
+        /// C7 タスクの色(ユーザー指定) (使わないかも)
         /// </summary>
         public string taskColor { set; get; }
         /// <summary>
-        /// C6 タスクの通知色設定(C2Notice側で制御)
+        /// C7 タスクの通知色設定(C2Notice側で制御)
         /// </summary>
         public string taskNoticeColor { set; get; }
     }
