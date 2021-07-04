20d4607507bab8d926ae6b86d169e69ce12641cb
argiltom <tomo0118@live.jp>
2021-07-04 12:31:30 +0900

    タスク完了確認画面のリサイズを禁止
    タスク完了確認画面のリサイズを禁止

--- TaskManagementApp/C2_AskTaskCompleateWindow.xaml.cs
+++ TaskManagementApp/C2_AskTaskCompleateWindow.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+//追加import 追記者 鈴木智仁
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 namespace TaskManagementApp
 {
     /// <summary>
@@ -19,6 +21,28 @@ namespace TaskManagementApp
     /// </summary>
     public partial class C2_TaskCompleateAskWindow : Window
     {
+        //7/2 例外排除の為,×ボタンの無効化 追記者 鈴木智仁
+
+        //externは「これが外部で実装されている」を意味する
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        const int GWL_STYLE = -16;
+        const int WS_SYSMENU = 0x80000;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            int style = GetWindowLong(handle, GWL_STYLE);
+            style = style & (~WS_SYSMENU);
+            SetWindowLong(handle, GWL_STYLE, style);
+        }
+
+        //*/7/2 例外排除の為,×ボタンの無効化 追記者 鈴木智仁
         Task task;
         public C2_TaskCompleateAskWindow(Task targetTask)
         {
